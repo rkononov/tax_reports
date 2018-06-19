fb08410207fb9fc46915ae17b086815cafd1e9cc
Roman Kononov <rkononov@gmail.com>
2018-06-20 00:12:05 +0600

    fixed script, migrated to roo gem

--- mkk_parse.rb
+++ mkk_parse.rb
@@ -1,25 +1,26 @@
-require "rubyXL"
+require "roo"
+require "roo-xls"
 require "csv"
 require "pry"
 
-def get_key_pair_values(sheet, offset, column_key, column_value, initial_offset = 14)
+def get_key_pair_values(sheet, offset, column_key, column_value, initial_offset = 15)
   i = initial_offset
   values = []
-  while sheet[i - offset] && sheet[i - offset][column_key] && sheet[i - offset][column_key].value
-    values << sheet[i - offset][column_key].value
-    values << sheet[i - offset][column_value].value
+  while sheet.cell(i - offset, column_key)
+    values << sheet.cell(i - offset, column_key)
+    values << sheet.cell(i - offset, column_value)
     i += 1
   end
   return values
 end
 
-def get_values(sheet, offset, column_value, initial_offset = 14)
+def get_values(sheet, offset, column_value, initial_offset = 15)
   i = initial_offset
   values = []
-  while sheet[i - offset] && sheet[i - offset][column_value] && sheet[i - offset][column_value].value &&
-        sheet[i - offset][column_value].value != "Близкий родственник" &&
-        !sheet[i - offset][column_value].value.to_s.start_with?("* должность указана")
-    values << sheet[i - offset][column_value].value
+  while sheet.cell(i - offset, column_value) &&
+        sheet.cell(i - offset, column_value) != "Близкий родственник" &&
+        !sheet.cell(i - offset, column_value).to_s.start_with?("* должность указана")
+    values << sheet.cell(i - offset, column_value)
     i += 1
   end
   return values
@@ -43,38 +44,44 @@ max_relative_outcome_length = 0
 
 Dir.glob("files/*.xlsx").each_with_index do |f, index|
   puts "Processing:#{f}, index: #{index}"
-  workbook = RubyXL::Parser.parse(f)
-  sheet = workbook[0]
-  year = f[6..9].to_i - 1
-  offset = 4 - (1..10).find { |x| sheet[x][0].value && sheet[x][0].value[0..11] == "Наименование" }
-  workplace = sheet[4 - offset][7].value
-  full_name = sheet[7 - offset][7].value
-  position = sheet[8 - offset][7].value
+  next if f[0..2] == "bad"
 
-  income = get_values(sheet, offset, 0)
+  workbook = begin
+               Roo::Spreadsheet.open(f)
+             rescue
+               Roo::Spreadsheet.open(f, extension: :xls)
+             end
+  sheet = (workbook.sheets.count > 3) ? workbook.sheet(workbook.sheets.count - 1) : workbook.sheet(0)
+  year = f[6..9].to_i - 1
+  offset = 5 - (1..10).find { |x| sheet.cell(x, 1) && sheet.cell(x, 1)[0..11] == "Наименование" }
+  workplace = sheet.cell(5 - offset, 8)
+  full_name = sheet.cell(8 - offset, 8)
+  position = sheet.cell(9 - offset, 8)
+
+  income = get_values(sheet, offset, 1)
   max_income_length = income.size if income.size > max_income_length
 
-  outcome = get_values(sheet, offset, 2)
+  outcome = get_values(sheet, offset, 3)
   max_outcome_length = outcome.size if outcome.size > max_outcome_length
 
-  real_estate = get_key_pair_values(sheet, offset, 4, 7)
+  real_estate = get_key_pair_values(sheet, offset, 5, 8)
   max_estate_length = real_estate.size if real_estate.size > max_estate_length
 
-  property = get_key_pair_values(sheet, offset, 13, 15)
+  property = get_key_pair_values(sheet, offset, 14, 16)
   max_property_length = property.size if property.size > max_property_length
 
-  relative_start_row = (10..100).find { |x| sheet[x] && sheet[x][0] && sheet[x][0].value == "Близкий родственник" }
+  relative_start_row = (10..100).find { |x| sheet.cell(x, 1) && sheet.cell(x, 1) == "Близкий родственник" }
 
-  relative_income = get_values(sheet, offset, 0, relative_start_row + 1 + offset)
+  relative_income = get_values(sheet, offset, 1, relative_start_row + 1 + offset)
   max_relative_income_length = relative_income.size if relative_income.size > max_relative_income_length
 
-  relative_outcome = get_values(sheet, offset, 2, relative_start_row + 1 + offset)
+  relative_outcome = get_values(sheet, offset, 3, relative_start_row + 1 + offset)
   max_relative_outcome_length = relative_outcome.size if relative_outcome.size > max_relative_outcome_length
 
-  relative_real_estate = get_key_pair_values(sheet, offset, 4, 7, relative_start_row + 1 + offset)
+  relative_real_estate = get_key_pair_values(sheet, offset, 5, 8, relative_start_row + 1 + offset)
   max_relative_estate_length = relative_real_estate.size if relative_real_estate.size > max_relative_estate_length
 
-  relative_property = get_key_pair_values(sheet, offset, 13, 15, relative_start_row + 1 + offset)
+  relative_property = get_key_pair_values(sheet, offset, 14, 16, relative_start_row + 1 + offset)
   max_relative_property_length = relative_property.size if relative_property.size > max_relative_property_length
   begin
     all_values << {f: f, year: year, workplace: workplace, position: position, full_name: full_name,
